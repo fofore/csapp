9a835e1af653b32c8c52517b64e7f2f332148a9d
fofore <qianminming@gmail.com>
2013-06-22 13:34:32 +0800

    commit this file

--- src/ex1.c
+++ src/ex1.c
@@ -5,5 +5,7 @@ int main(int argc, char **argv) {
 	printf("here is the %d arguments: %s\n", argc, *argv);
 	printf("the argument 2 is %s\n", argv[argc-1]);
 	printf("the argument 2 is %s\n", argv[argc]);
+
+	printf("do the commit test");
 	return 0;
 }
